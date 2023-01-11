41884d3bb0f3c9d884b4680fe2204767f994ba39
“Cory <coryurton@gmail.com>
2023-01-10 22:59:18 -0600

    cannot display text without Text component proof

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+      Hello World!
     </View>
   );
 }
@@ -22,4 +20,4 @@ const styles = StyleSheet.create({
 
 
 
-// reactnative.dev/docs/components-and-apis
+// reactnative.dev/docs/components-and-apis   view text button image textinput
